6b07424b3d45443ca2b2216b4c8aa4dc7415a6fb
Benjamin Lee <benjamin.lee.2021@gmail.com>
2022-07-04 11:13:39 +0930

    feat(readfolder): created a function that will take in a folder path then returns folders/files

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	// "flag"
-	"fmt"
+	"errors"
+	"os"
 )
 
-func main() {
-	fmt.Println("Benjis Mac Cleaner")
+func readFolder(dir string) (fileArray []string, err error) {
+
+	var fileArry []string
+	f, err := os.Open(dir)
+
+	if err != nil {
+		return fileArry, errors.New("error opening dir")
+	}
+
+	files, err := f.Readdir(-1)
+	f.Close()
+
+	if err != nil {
+		return fileArry, errors.New("error reading dir")
+	}
+
+	for _, file := range files {
+		fileArry = append(fileArry, file.Name())
+	}
+	return fileArry, nil
 }
